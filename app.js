f7bb99828ce48e1ea6a6db7c2b3c298f0e25bfbb
Nathan Ramos Rezende <124633138+rezys7@users.noreply.github.com>
2024-04-13 14:07:17 -0300

    routes & views

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }))
+//carregar html
+app.set('views' , path.resolve(__dirname , 'src' , 'views'))
+app.set('view engine' , 'ejs')
+app.use(routes)
+
+
+
+app.listen(3000 , () => {
+    console.log('click here -> https://localhost:3000')
+})
